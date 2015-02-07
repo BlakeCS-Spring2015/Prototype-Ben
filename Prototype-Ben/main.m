96594b0fae75c8115dcbd58be4206f523474d6cd
Ben Weinshel <weinshel@users.noreply.github.com>
2015-02-06 21:04:07 -0600

    Initial Commit

---
+++ Prototype-Ben/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Prototype-Ben
+//
+//  Created by Ben Weinshel on 2/6/15.
+//  Copyright (c) 2015 Ben Weinshel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
