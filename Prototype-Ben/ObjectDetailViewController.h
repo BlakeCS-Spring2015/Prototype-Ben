31a495050563edbd493322e240355cbc0b722b20
Ben Weinshel <weinshel@users.noreply.github.com>
2015-02-08 20:18:22 -0600

    added table view, some new controllers

---
+++ Prototype-Ben/ObjectDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ObjectDetailViewController.h
+//  Prototype-Ben
+//
+//  Created by Ben Weinshel on 2/8/15.
+//  Copyright (c) 2015 Ben Weinshel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ObjectDetailViewController : UIViewController
+
+@end
